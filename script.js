1502bdfb7ba75a16ef5281939caae111c572a3be
unknown <brianna.albo@gmail.com>
2022-12-08 15:47:55 -0500

    adds api key and geolocation to javascript file, able to pull coordinates from user's current location to pull data from api

---
+++ script.js
@@ -0,0 +1,34 @@
+const key = "599bfde9dad9e03d506db251b686645c";
+
+if ('geolocation' in navigator) {
+    navigator.geolocation.getCurrentPosition(setPosition);
+}
+else {
+    notificationElement.style.display = 'block';
+    notificationElement.innerHTML = `<p> Browser doesn't support Geolocation`;
+}
+
+function setPosition(position) {
+    let latitude = position.coords.latitude;
+    let longitude = position.coords.longitude;
+
+    console.log(latitude);
+    console.log(longitude);
+
+    getWeather(latitude, longitude);
+
+}
+
+function getWeather(latitude, longitude) {
+    let api = `http://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&appid=${key}`;
+    console.log(api);
+    
+    fetch(api)
+    .then(function(response) {
+    let data= response.json();
+    return data;
+    });
+    // .then(function(data) {
+    // getWeather.temperature.value = Math.floor(data.main.temp - Kelvin)
+// });
+}
